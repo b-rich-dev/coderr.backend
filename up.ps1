3943f1c7dbc23f375f00306943c514d41b942e8d
b-rich-dev <eugen.birich@googlemail.com>
2026-04-16 14:13:18 +0200

    test: deployment pipeline test 4

--- up.ps1
+++ up.ps1
@@ -4,4 +4,4 @@ git pull
 git add .
 git commit -m $message
 git push
-gcloud compute ssh eugen_birich@coderr-server --zone=europe-west3-c --command="cd ~/coderr.backend && git pull && docker compose up --build -d"
+& "$env:LOCALAPPDATA\Google\Cloud SDK\google-cloud-sdk\bin\gcloud.ps1" compute ssh eugen_birich@coderr-server --zone=europe-west3-c --command="cd ~/coderr.backend && git pull && docker compose up --build -d"
