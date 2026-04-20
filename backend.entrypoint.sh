6a9f5856f7759b111b754baedb26cc2657b2661b
b-rich-dev <eugen.birich@googlemail.com>
2026-04-20 11:20:23 +0200

    fix: remove rqworker and gunicorn reload

--- backend.entrypoint.sh
+++ backend.entrypoint.sh
@@ -60,6 +60,4 @@ for guest in guest_users:
         print(f"Guest user '{guest['username']}' already exists.")
 EOF
 
-python manage.py rqworker default &
-
-exec gunicorn core.wsgi:application --bind 0.0.0.0:8000 --reload
+exec gunicorn core.wsgi:application --bind 0.0.0.0:8000
